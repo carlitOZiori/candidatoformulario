6d86756880fce339d8dd058e28ec0600eaa94b66
carlitOZiori <163367901+carlitOZiori@users.noreply.github.com>
2024-03-13 21:19:57 -0600

    registro.php

--- Proyecto/registro.php
+++ Proyecto/registro.php
@@ -20,4 +20,25 @@ var_dump($nombre);
 $rta mail('carlosalbert.pitufo0270@gmail.com',$apellido_ma, $apellido_ma, $nombre, $email);
 var_dump($rta);
 ?>
+<?php
 
+if(! isset( $_POST['REQUEST_METHOD'] != 'POST') ){
+    header("Location: index.html" );
+}
+die();
+
+$nombre = $_POST['nombre'];
+$apellido_pa = $_POST['apellido_paterno'];
+$apellido_ma = $_POST['apellido_materno'];
+$email = $_POST['email'];
+
+$body = <<<HTML
+   <h1>Candidato tus datos son:</h1>
+   <p>De: $nombre $apellido_pa $apellido_ma / $email</p>
+HTML;
+
+var_dump($nombre);
+
+$rta mail('carlosalbert.pitufo0270@gmail.com',$apellido_ma, $apellido_ma, $nombre, $email);
+var_dump($rta);
+?>
